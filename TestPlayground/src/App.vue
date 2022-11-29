3925efcb9cd14387197ca437512cdf43c33b11f7
qiuye <1302580865@qq.com>
2022-11-29 14:55:27 +0800

    updata: TestPlayground

--- TestPlayground/src/App.vue
+++ TestPlayground/src/App.vue
@@ -1,8 +1,56 @@
-<script setup lang="ts">
-// import demo from '../../packages/core/useMedia/demo.vue';
-import demo from '../../packages/core/useLocalStorage/demo.vue';
+<script setup lang="ts">// @ts-nocheck
+import { ref } from 'vue';
+import Side from './components/Side.vue';
+import useMedia from '../../packages/core/useMedia/demo.vue';
+import useLocalStorage from '../../packages/core/useLocalStorage/demo.vue';
+
+const tableList = [
+    {
+        name: 'useMedia',
+        com: useMedia
+    },
+    {
+        name: 'useLocalStorage',
+        com: useLocalStorage
+    }
+];
+const active = ref(0);
+const change = index => {
+    active.value = index;
+};
 </script>
 
 <template>
-    <demo />
+    <div class="container">
+        <div class="side">
+            <Side
+                v-for="(item, index) in tableList"
+                :key="item.name"
+                :name="item.name"
+                @click="change(index)"
+            />
+        </div>
+        <div class="content">
+            <component :is="tableList[active].com" />
+        </div>
+    </div>
 </template>
+<style lang="css" scoped>
+.container {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+}
+.side {
+    min-width: 100px;
+    box-shadow: 0 0 3px 1px black;
+    overflow: hidden;
+}
+.content {
+    flex-grow: 4;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    padding-top: 2rem;
+}
+</style>
---
+++ TestPlayground/src/components/Side.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const props = defineProps<{
+    name: string
+}>()
+</script>
+<template>
+    <div class="tab">
+        {{ props.name }}
+    </div>
+</template>
+<style lang="css" scoped>
+.tab {
+    user-select: none;
+    cursor: pointer;
+    margin: 0.5rem 0.2rem;
+}
+</style>
